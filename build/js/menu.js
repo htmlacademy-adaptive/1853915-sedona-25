905391fef58964bfd6173dc719051059c29e937c
Artem <kazuta.artem@gmail.com>
2022-05-21 05:44:20 +0300

    build2

--- build/js/menu.js
+++ build/js/menu.js
@@ -1,1 +1,23 @@
-let menu=document.querySelector(".main-nav__list"),button_open=document.querySelector(".main-nav__toggle"),button_close=document.querySelector(".main-nav__cross");const mediaQuery=window.matchMedia("(min-width: 768px)");function basket(){menu.classList.add("display_menu")}function close_basket_catalog(){menu.classList.remove("display_menu")}function handleTabletChange(e){e.matches&&menu.classList.remove("display_menu")}button_open.addEventListener("click",basket),button_close.addEventListener("click",close_basket_catalog),mediaQuery.addListener(handleTabletChange),handleTabletChange(mediaQuery);
+let menu = document.querySelector(".main-nav__list");
+let button_open = document.querySelector(".main-nav__toggle");
+let button_close = document.querySelector(".main-nav__cross");
+const mediaQuery = window.matchMedia('(min-width: 768px)')
+
+button_open.addEventListener("click", basket);
+
+function basket() {
+  menu.classList.add("display_menu");
+}
+button_close.addEventListener("click", close_basket_catalog);
+
+function close_basket_catalog() {
+  menu.classList.remove("display_menu");
+}
+
+function handleTabletChange(e) {
+  if (e.matches) {
+    menu.classList.remove("display_menu");
+  }
+}
+mediaQuery.addListener(handleTabletChange)
+handleTabletChange(mediaQuery)
