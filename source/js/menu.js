a3ee284c9eab7100c85c9dcf8a511ee7b50102f0
Artem <kazuta.artem@gmail.com>
2022-05-29 17:29:22 +0300

    unluck

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,22 +1,26 @@
-let menu = document.querySelector(".main-nav__list");
+let menu = document.querySelector("ul");
 let button_open = document.querySelector(".main-nav__toggle");
 let button = document.querySelector("button");
 let button_close = document.querySelector(".main-nav__cross");
 const mediaQuery = window.matchMedia('(min-width: 768px)')
 
-button.addEventListener("click", basket);
+menu.classList.remove("main-nav--open");
+menu.classList.add("main-nav--close");
+button_open.style.display = "block";
+button_close.style.display = "block";
+
+
+button_open.addEventListener("click", basket);
 
 function basket() {
-  button.classList.toggle("main-nav__toggle")
-  button.classList.toggle("main-nav__cross")
-  menu.classList.toggle("display_menu");
+  button.classList.add("main-nav__cross");
 }
 
 function handleTabletChange(e) {
   if (e.matches) {
-    menu.classList.remove("display_menu");
-    button.classList.remove("main-nav__cross")
-    button.classList.add("main-nav__toggle")
+    // menu.classList.remove("display-menu");
+    // button.classList.remove("main-nav__cross")
+    // button.classList.add("main-nav__toggle")
   }
 }
 mediaQuery.addListener(handleTabletChange)
